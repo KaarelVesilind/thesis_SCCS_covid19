a9a99e2b7841da24413c65a0fb4a92d4be885a9f
KaarelVesilind <43817181+KaarelVesilind@users.noreply.github.com>
2022-09-07 08:21:23 +0300

    Update SCCS_pipeline.R

--- code/SCCS_pipeline.R
+++ code/SCCS_pipeline.R
@@ -1,7 +1,14 @@
-# SCCS study pipeline
-
 ### Database connections
 library(DatabaseConnector)
+
+#------ VARIABLES -------
+#Vaadeldav kohort 
+exposureId = 191
+#Tulemustefaili nimi
+resultsFile = "results191_level5_firstusage.RData"
+#ATC level
+ATCfile = "ATC_level5.json"
+#------------------------
 
 Sys.setenv(DATABASECONNECTOR_JAR_FOLDER = "/home/kvesilind/Jdbc")
 downloadJdbcDrivers(dbms = "postgresql")
@@ -29,31 +36,26 @@ databaseName <- "University of Tartu"
 databaseDescription <- "National health insurance claims for all Estonian COVID-19 cases before February 2021 (~56K cases) and four controls (randomly selected form population) per every case. The database was updated in Nov 2021 and ~25K controls also contracted COVID-19 between February 2021 and November 2021."
 cdmVersion <- 5.3
 
-
 #------------------------------------
-# ATC codes for declared level
+# ATC koodide tõmbamine ja neist listi tegemine
 library("rjson")
-myData <- fromJSON(file="ATC_level2.json")
+myData <- fromJSON(file=ATCfile)
 ATCcodes = list()
 for (i in myData) {
   ATCcodes = append(ATCcodes, i$concept_code)
 }
-
 #-----------------------------------------
+length(ATCcodes)
+#Uuringu andmete salvestamine soovitud kujule.
 
 resultList = list()
 library(SelfControlledCaseSeries)
-
-# Exposure cohort ID
-exposureId = 165 
-
-# Loop for studying every ATC code
 for (outcomeId in 1: length(ATCcodes)){
   print(outcomeId)
   sccsData <- getDbSccsData(connectionDetails = connectionDetails,
                             cdmDatabaseSchema = cdmDatabaseSchema,
                             outcomeDatabaseSchema =  "kvesilind", 
-                            outcomeTable = "COHORT_level2",
+                            outcomeTable = "COHORT_level5_firstusage",
                             outcomeIds = outcomeId, 
                             exposureDatabaseSchema = cohortDatabaseSchema,
                             exposureTable = "COHORT",
@@ -62,11 +64,11 @@ for (outcomeId in 1: length(ATCcodes)){
   studyPop <- createStudyPopulation(sccsData = sccsData,
                                     outcomeId = outcomeId,
                                     firstOutcomeOnly = FALSE,
-                                    naivePeriod = 180)
+                                    naivePeriod = 365)
   covarExposureSplit <- createEraCovariateSettings(label = "Exposure of interest",
                                                    includeEraIds = exposureId,
                                                    start = -30,
-                                                   end = 10000, # Big number so the period is till the end of observation period
+                                                   end = 10000, # Suur number, et long-covid oleks andmebaasis oleva perioodi lõpuni
                                                    endAnchor = "era start",
                                                    splitPoints = c(30, 180))
   sccsIntervalData <- createSccsIntervalData(studyPopulation = studyPop,
@@ -74,11 +76,15 @@ for (outcomeId in 1: length(ATCcodes)){
                                              eraCovariateSettings = covarExposureSplit)
   if (summary(sccsIntervalData)$caseCount != 0) {
     model <- fitSccsModel(sccsIntervalData)
-    # Save the results to list
-    resultList = append(resultList, list(c(ATCcodes[outcomeId], getModel(model))))
+    # Salvestame tulemuse hilisemaks analüüsiks listi. Vahepeal mudel ei suuda fittida ja tuleb katkine mudel, selleks try
+    try({
+      resultList = append(resultList, list(c(ATCcodes[outcomeId], getModel(model))))
+    })
   }
 }
+#getModel(model)
+
 
 # ------------------------------------------
-# Save results to file
-saveRDS(resultList, file="results165RespiratoryAgeOver18_level2_period2.RData")
+# Salvestame tulemused hilisemaks, et ei peaks uuesti uuringut läbi jooksutama
+saveRDS(resultList, file=resultsFile)
